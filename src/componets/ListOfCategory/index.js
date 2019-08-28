f645aab18cbd185639b1cf152995ed2127c9efdf
William De la Espriella <wdelaespriella@hotmail.com>
2019-08-27 20:25:52 -0500

    use useEffect in scroll

--- src/componets/ListOfCategory/index.js
+++ src/componets/ListOfCategory/index.js
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, Fragment } from 'react'
 import {Category} from '../Category'
 import {  List, Item } from "./styles";
 // import { categories as mockCategories }  from '../../../api/db.json'
 
 export const ListOfCategory = ()=>{
     const [categories, setCategories] = useState([])
-
+    const [showFixed, setShowFixed] = useState(false)
 
    useEffect(()=>{
        window.fetch('https://petgram-api-example.now.sh/categories')
@@ -14,12 +14,31 @@ export const ListOfCategory = ()=>{
            setCategories(response)
        })
    },[])
-return(
-    <List>
+
+   useEffect(()=>{
+       
+       const onScroll= e =>{
+           const newShowFixed = window.scrollY > 200 
+           showFixed != newShowFixed && setShowFixed(newShowFixed)
+       }
+       document.addEventListener('scroll', onScroll)
+       return ()=> document.removeEventListener('scroll',onScroll)
+   },[showFixed])
+
+   const renderList =(fixed )=>(
+       <List className= {fixed ? 'fixed' : ''}>
         {
             categories.map(category => (<Item key = {category.id}> <Category { ...category }/></Item>))
         }
     </List>
+   )
+
+return(
+    <Fragment>
+        {renderList()}
+        {showFixed && renderList(true)}
+    </Fragment>
+
 );
 }
 
